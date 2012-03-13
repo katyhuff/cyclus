36827716715d9e7b81f9a5d428e2e9c5ce6aa581
Katy Huff <katyhuff@gmail.com>
2012-03-13 15:22:04 -0500

    added documentation, etc.

--- src/Models/Facility/ToasterFacility/ToasterFacility.cpp
+++ src/Models/Facility/ToasterFacility/ToasterFacility.cpp
@@ -3,6 +3,7 @@
 
 #include "ToasterFacility.h"
 
+#include <list>
 #include "Logger.h"
 #include "CycException.h"
 #include "InputXML.h"
@@ -26,6 +27,32 @@ void ToasterFacility::init(xmlNodePtr cur) {
   /// move XML pointer to current model
   cur = XMLinput->get_xpath_element(cur,"model/ToasterFacility");
   /// initialize any ToasterFacility-specific datamembers here
+  n_slices_ = strtol(XMLinput->get_xpath_content(cur, "rate"), NULL, 10);
+  toastiness_ = XMLinput->get_xpath_content(cur,"toastiness");
+  rate_ = strtod(XMLinput->get_xpath_content(cur, "rate"), NULL);
+  incommodity_ = XMLinput->get_xpath_content(cur, "incommodity");
+  outcommodity_ = XMLinput->get_xpath_content(cur, "outcommodity");
+
+  // check that toastiness_ is oneof the allowed levels :
+  // this gives an example of performing input checking in the module 
+  // in case the xml parser is not detailed enough
+  set<string> allowed_levels;
+  allowed_levels.insert("light");
+  allowed_levels.insert("golden");
+  allowed_levels.insert("dark");
+  allowed_levels.insert("burnt");
+  if(allowed_levels.find(toastiness_)==allowed_levels.end()){
+    string msg = "The value given for the darkenss parameter, ";
+    msg += toastiness_;
+    msg += ", is not within the allowed set. Allowed values are: ";
+    set<string>::iterator it;
+    for (it=allowed_levels.begin(); it != allowed_levels.end(); it++){
+      msg += " ";
+      msg += (*it);
+    }
+    msg+=".";
+    throw CycException(msg);
+  }
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
