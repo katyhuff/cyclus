65c5d60fff2d8c0e8056b9d035e4bc695d77069e
Katy Huff <katyhuff@gmail.com>
2012-03-20 19:30:15 -0500

    copy-paste error.

--- src/Models/Facility/ToasterFacility/ToasterFacility.cpp
+++ src/Models/Facility/ToasterFacility/ToasterFacility.cpp
@@ -39,7 +39,7 @@ void ToasterFacility::init(xmlNodePtr cur) {
   cur = XMLinput->get_xpath_element(cur,"model/ToasterFacility");
 
   /// initialize any ToasterFacility-specific datamembers here
-  n_slices_ = strtol(XMLinput->get_xpath_content(cur, "rate"), NULL, 10);
+  n_slices_ = strtol(XMLinput->get_xpath_content(cur, "nSlices"), NULL, 10);
   toastiness_ = XMLinput->get_xpath_content(cur,"toastiness");
   rate_ = strtod(XMLinput->get_xpath_content(cur, "rate"), NULL);
   incommodity_ = XMLinput->get_xpath_content(cur, "incommodity");
@@ -164,10 +164,12 @@ void ToasterFacility::handleTock(int time) {
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void ToasterFacility::makeRequests() { 
+
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void ToasterFacility::makeOffers() { 
+
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
